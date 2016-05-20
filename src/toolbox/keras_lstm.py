4cb73eea107ba488868c8e61714c0724b2df4201
Siebert Looije <looijesiebert@gmail.com>
2016-05-20 11:09:22 +0200

    added keras_lstm

--- src/toolbox/keras_lstm.py
+++ src/toolbox/keras_lstm.py
@@ -69,11 +69,11 @@ label_dict = {'!': 1, '#': 2, '$': 3, '&': 4, '*': 5, ',': 6, '-': 7, '.': 8, '4
 word_log = {}
 
 
-def extractImages(wordfile, imgfile, l_file):
+def extractImages(wordfile, imgfile):
     print wordfile
     lines, _ = wordio.read(wordfile)
     img = Image.open(imgfile)
-    # img = pamImage.PamImage(imgfile)
+    #img = pamImage.PamImage(imgfile
 
     # line_iter = iter(lines)
     # cur_line = line_iter.next()
@@ -84,17 +84,29 @@ def extractImages(wordfile, imgfile, l_file):
     out_str = out_str.replace(IM_EXT, '')
     train_y = []
     for line_idx, line in enumerate(lines):
-
         for word_idx, word in enumerate(line):
-            word_array = np.zeros((word.right-word.left))
 
+            cropped = img.crop((word.left, word.top, word.right, word.bottom))  # croplib.crop(img, word.left, word.top, word.right, word.bottom)
+
+            dict_name = out_str +'_l' + str(line_idx) + '_w' + str(word_idx) + '_t_' + word.text
+            out_path = out_str + '_l' + str(line_idx) + '_w' + str(word_idx) + '_t_' + word.text + IM_EXT
+            word_array = np.zeros((word.right - word.left))
+            gaussian_array = {}
+            cropped.save(out_path)
             for idx, char in enumerate(word.characters):
-
-                word_array[char.left - word.left] = 1
+               # if(char.left - word.left >= 0):e
+               if(char.left - word.left < 0):
+                   word_array[0] = 1
+               elif((char.left-word.left)>=(word.right-word.left)):
+                   word_array[(char.left - word.left)-1] = 1
+               else:
+                   word_array[(char.left - word.left)] = 1
 
             word_array[(word.right-word.left)-1] = 1
 
-            gaussian_array = gaussian_filter1d(word_array,0.5)
+            gaussian_array[dict_name] = gaussian_filter1d(word_array,0.5)
+           # pyplot.plot(gaussian_array)
+           # pyplot.show()
             train_y.append(gaussian_array)
 
             """
@@ -148,124 +160,123 @@ def extractImages(wordfile, imgfile, l_file):
                 if char.text.lower() in label_dict:
                     l_file.wri te(out_str + ' ' + str(label_dict[char.text.lower()] - 1) + '\n')
                     """
+
     return train_y
 
 #https://github.com/fchollet/keras/issues/401
 if __name__ == "__main__":
 
-    label_file = open('labels.txt', 'w')
-    label_file.write('# label file\n')
-
+#    label_file = open('labels.txt', 'w')
+#    label_file.write('# label file\n')
     print 'Cropping...'
     assert pth.exists('../charannotations/KNMP') and pth.exists('../charannotations/Stanford')
 
+    train_y =[]
 
+    counter = 0
     for f in listdir('../charannotations/KNMP'):
         wordfile = '../charannotations/KNMP/' + f
         imgfile = wordfile.replace('.words', IM_EXT).replace('/charannotations', '/pages').replace('2C20', '2C2O')
 
-        train_y = extractImages(wordfile, imgfile, label_file)
+        train_y.extend(extractImages(wordfile, imgfile))
 
     for f in listdir('../charannotations/Stanford'):
         wordfile = '../charannotations/Stanford/' + f
         imgfile = wordfile.replace('.words', IM_EXT).replace('/charannotations', '/pages').replace('2C20', '2C2O')
 
-        train_y =  extractImages(wordfile, imgfile, label_file)
+        train_y.extend(extractImages(wordfile, imgfile))
 
     feature_data = {}
 
     features = []
     labels = []
-    for i, line in enumerate(open('../toolbox/labels.txt')):
-        path, label = line.split()
-        try:
-            im = np.asarray(Image.open(path.replace('/crops', '/crops/letters')), dtype=np.float32)
-            if im.shape[0] < 10 or im.shape[1] < 10:
+    #for i, line in enumerate(open('../toolbox/labels.txt')):
+    #    path, label = line.split()
+    #    try:
+    train_x = []
+    max_height = 0
+    max_width = 0
+    for i in range(0,len(train_y)):
+        for key in train_y[i]:
+            path = '../crops/'+str(key)+ IM_EXT
+
+            try:
+                im = np.asarray(Image.open(path), dtype=np.float32)
+                if im.shape[0] < 10 or im.shape[1] < 10:
+                    continue
+            except IOError:
+                print(path + ' gives trouble...')
                 continue
-        except IOError:
-            print(path + ' gives trouble...')
-            continue
 
-        im = im[:, :, ::-1]
-        im[:, :, 0] -= 103.939
-        im[:, :, 1] -= 116.779
-        im[:, :, 2] -= 123.68
-
-        im = im.transpose((2, 0, 1))
-
-        # run lasagne
-        print "Computing keras result... Im shape:", im.shape,
-
-        keras_result = model.predict(im.reshape((1, im.shape[0], im.shape[1], im.shape[2])))
-        print 'result shape', keras_result.shape
-        features.append(np.mean(keras_result, axis=(2)).transpose((0, 2, 1)))
-        labels.append(int(label))
+            labels.append(train_y[i][key])
+            im = im[:, :, ::-1]
+            im[:, :, 0] -= 103.939
+            im[:, :, 1] -= 116.779
+            im[:, :, 2] -= 123.68
 
 
+            im = im.transpose((1,0,2))
+            max_width = max(max_width, len(im[:,0,0]))
+            max_height = max(max_height, len(im[0, :, 0]))
+            new_image = np.pad(im, pad_width=((0,504-im.shape[0]),(0,511-im.shape[1]),(0,0)),mode='constant',constant_values=0)
+            new_image = new_image.reshape(new_image.shape[0], new_image.shape[1]*new_image.shape[2])
+            #print new_image.shape
+            train_x.append(new_image)
+    #Klopt
+    print(train_x[0].shape)
+    print(labels[0].shape)
 
-
-
-    print 'done cropping'
-    print 'overview of words: '
-
-    """
-    print dataX[0].shape
-
-
-    data = zip(dataX, dataY)
+    data = zip(train_x, labels)
     random.shuffle(data)
 
     dataX, dataY = zip(*data)
-    #dataX = np.asarray(dataX)
-    rawY = dataY
-    dataY = to_categorical(np.asarray(dataY))
+    dataX = np.asarray(dataX)
+    print(dataX[0].shape)
+    dataY = np.asarray(dataY)
+
+    print(dataY[0].shape)
+    #rawY = dataY
+    #dataY = to_categorical(np.asarray(dataY))
 
     #Need input: (input_length,time_steps,n_dim)
     #dataX = np.reshape(dataX,(len(dataX),1,512))
 
     #print dataX.shape
     #print dataY.shape
-    dataX,maxlen = pad_sequences(dataX,dim=512)
+    #dataX,maxlen = pad_sequences(dataX,dim=512)
 
-    trainX = dataX[:14000]
-    print trainX[0].shape
-    trainY = dataY[:14000]
+    trainX = dataX[:10]
+    trainY = dataY[:10]
 
-    testX = dataX[14000:]
-    testY = dataY[14000:]
+    testX = dataX[4000:]
+    testY = dataY[4000:]
 
     print "Building up model"
 
-    model = Sequential()
+
     #Input shape = (time_steps, n_dim)
-    left = Sequential()
-    left.add(LSTM(output_dim=512, return_sequences=True,
-            input_shape=(maxlen, 512)))
-    right = Sequential()
-    right.add(LSTM(output_dim=512, return_sequences=True,
-                   input_shape=(maxlen,512), go_backwards=True))
 
-    model.add(Merge([left, right], mode='concat'))
+    model = Sequential()
+    model.add(LSTM(504, return_sequences=True,input_shape=(504,1533)))
 
-    #model.add(LSTM(512, return_sequences=True,  input_shape=(1,512)))
+    #model.add(Dropout(0.2))
 
-    model.add(Dropout(0.2))
 
     #Maybe thinking about return_sequences= True but we need then TimeDistributedDense
     #Or we can do both LSTM next to each other and then merge them together.
-    model.add(LSTM(512, return_sequences=False))
-    model.add(Dense(dataY.shape[1]))
-    model.add(Activation('softmax'))
+    #model.add(LSTM(511, return_sequences=False))
+    #model.add(Dense(dataY.shape[1]))
+    #model.add(Activation('softmax'))
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
     print "Compiled model"
-    model.compile(loss='categorical_crossentropy',
+    model.compile(loss='mean_absolute_error',
                   optimizer='rmsprop',
                   metrics=['accuracy'])
 
     print "Train model"
-    model.fit([trainX,trainX], trainY,batch_size=32,validation_split=0.1, nb_epoch=1000)
+    model.fit(trainX, trainY,batch_size=1,validation_split=0.1, nb_epoch=1000)
     print "Test model"
-    score,accuracy = model.evaluate([testX,testX],testY,batch_size=32)
-    print 'accuracy : ' + str(accuracy)  """
+    score,accuracy = model.evaluate(testX,testY,batch_size=1)
+    print 'accuracy : ' + str(accuracy)
